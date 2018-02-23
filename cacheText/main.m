f752f0e84f2310f28d251911ef0338f1f9e3d9c5
wanglei <wanglei@9086.cn>
2018-02-23 10:41:29 +0800

    Initial Commit

---
+++ cacheText/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  cacheText
+//
+//  Created by wanglei on 2018/2/23.
+//  Copyright © 2018年 wanglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
